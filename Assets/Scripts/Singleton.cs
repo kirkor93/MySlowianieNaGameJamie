8115db194e6562927a9fe3edfa7391e578d6164e
Patryk Kochański <patryk.kochanski@hotmail.com>
2015-07-24 17:57:35 +0200

    Adding Singleton

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<T>();
+
+                if (instance == null)
+                {
+                    GameObject singleton = new GameObject();
+                    instance = singleton.AddComponent<T>();
+                    singleton.name = "(singleton) " + typeof(T).ToString();
+                }
+            }
+
+            return instance;
+        }
+    }
+
+    public virtual void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
+
--- Assets/Scripts/TestScript.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class TestScript : MonoBehaviour 
-{
-    public void Dupa()
-    {
-        
-    }
-}
