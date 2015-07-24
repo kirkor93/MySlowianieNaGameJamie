a9f95eb5166aab94b51d197699e5f1c55b9534a0
Patryk Kochański <patryk.kochanski@hotmail.com>
2015-07-24 23:33:18 +0200

    Game Loop mechanics is pretty cool, added change game period, attached event on change period and its pretty nice :3

--- Assets/Scripts/VillageController.cs
+++ Assets/Scripts/VillageController.cs
@@ -3,10 +3,35 @@ using System.Collections;
 
 public class VillageController : Singleton<VillageController> {
 
-    private int _woodValue = 0;
-    private int _foodValue = 0;
-    private int _ironValue = 0;
-    private int _stoneValue = 0;
+    public float VillageHP
+    {
+        get;
+        set;
+    }
+
+    public int WoodValue
+    {
+        get;
+        set;
+    }
+
+    public int FoodValue
+    {
+        get;
+        set;
+    }
+
+    public int IronValue
+    {
+        get;
+        set;
+    }
+
+    public int StoneValue
+    {
+        get;
+        set;
+    }
 
     public int WoodNeededValue
     {
@@ -40,26 +65,6 @@ public class VillageController : Singleton<VillageController> {
 	// Update is called once per frame
 	void Update () {
         //Delete debug when informations will be on GUI
-        Debug.Log(string.Format("Food {0} / Iron {1} / Stone {2} / Wood {3} /", _foodValue, _ironValue, _stoneValue, _woodValue));
+        Debug.Log(string.Format("Food {0} / Iron {1} / Stone {2} / Wood {3} /", FoodValue, IronValue, StoneValue, WoodValue));
 	}
-
-    public void IncreaseWood(int value)
-    {
-        _woodValue += value;
-    }
-
-    public void IncreaseFood(int value)
-    {
-        _foodValue += value;
-    }
-
-    public void IncreaseIron(int value)
-    {
-        _ironValue += value;
-    }
-
-    public void IncreaseStone(int value)
-    {
-        _stoneValue += value;
-    }
 }
