ae9c2f4d214c0eda84f850918c51fdc6c699cac8
Krzysztof Stengert <krzysztof.stengert@hotmail.com>
2015-07-24 17:54:57 +0200

    Basic tower

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tower : MonoBehaviour 
+{
+    private float _timer = 0.0f;
+
+    protected GameObject _enemy;
+
+    [SerializeField]
+    protected float _damage;
+    [SerializeField]
+    protected float _attackCooldown;
+    [SerializeField]
+    protected LayerMask _enemyLayer;
+
+    public float Damage
+    {
+        get { return _damage; }
+        set { _damage = value; }
+    }
+
+    void Update()
+    {
+        _timer += Time.deltaTime;
+        if (_timer >= _attackCooldown)
+        {
+            _timer = 0.0f;
+            Attack();
+        }
+    }
+
+    public void Attack()
+    {
+        Debug.Log("Try attack");
+        if (_enemy == null)
+        {
+            Debug.Log("Nothing to attack");
+            return;
+        }
+        Debug.Log("Attack");
+        //Attack this fucking enemy
+    }
+
+    protected void OnTriggerEnter(Collider col)
+    {
+        if(_enemy != null)
+        {
+            return;
+        }
+        else if (col.gameObject.layer == _enemyLayer)
+        {
+            _enemy = col.gameObject;
+        }
+    }
+
+    protected void OnTriggerExit(Collider col)
+    {
+        if (_enemy == null) return;
+        if (col.gameObject.GetInstanceID() == _enemy.GetInstanceID())
+        {
+            _enemy = null;
+        }
+    }
+}
---
+++ Assets/Scripts/TowerSlot.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerSlot : MonoBehaviour
+{
+    public GameObject Tower;
+    public GameObject Cannon;
+    public GameObject Mortar;
+
+    private bool _isPlayerCloseEnough = false;
+
+    void Update()
+    {
+        
+    }
+}
