5ed2aa7f9ab67dbbe887bdb5a6e694181e9ff1bf
Krzysztof Stengert <krzysztof.stengert@hotmail.com>
2015-07-25 20:00:43 +0200

    Camera when collecting resources is just wow!

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour 
+{
+    public Transform[] Targets;
+    public float MinimalDistance = 15.0f;
+    public float CameraMovingSpeed = 0.05f;
+
+    protected Vector3 _sum;
+    protected int _activePlayers;
+    protected Vector3 _farest;
+    protected Camera _camera;
+
+    void Awake()
+    {
+        Invoke("CalculatePlayers", 0.5f);
+        _camera = GetComponent<Camera>();
+    }
+
+    void CalculatePlayers()
+    {
+        foreach (Transform t in Targets)
+        {
+            if (t.gameObject.activeSelf)
+            {
+                ++_activePlayers;
+            }
+        }
+    }
+
+    void Update()
+    {
+        _sum = Vector3.zero;
+        foreach(Transform t in Targets)
+        {
+            if (t.gameObject.activeSelf)
+            {
+                _sum += t.position;
+            }
+        }
+        Vector3 center = _sum / _activePlayers;
+        float farestDistance = 0.0f;
+        foreach(Transform t in Targets)
+        {
+            if(t.gameObject.activeSelf)
+            {
+                float dist = Vector3.Distance(t.position, center);
+                if (dist > farestDistance)
+                {
+                    farestDistance = dist;
+                    _farest = t.position;
+                }
+            }
+        }
+        Vector3 backVector = (Vector3.up + Vector3.back) * (MinimalDistance + farestDistance);
+        backVector = Quaternion.AngleAxis(transform.rotation.x, Vector3.right) * backVector;
+        transform.position = Vector3.Lerp(transform.position, center + backVector, CameraMovingSpeed);
+    }
+}
---
+++ Assets/WorkScripts/ResourceSetter.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResourceSetter : MonoBehaviour {
+
+    public Transform Corner1;
+    public Transform Corner2;
+    public GameObject FoodResource;
+    public GameObject WoodResource;
+    public GameObject IronResource;
+    public GameObject StoneResource;
+    public int MaxResources = 20;
+
+    [ContextMenu("Generate resources")]
+    void Generate()
+    {
+        if(Corner1 == null || Corner2 == null)
+        {
+            Debug.LogError("How can I create null or random between nulls");
+            return;
+        }
+        GameObject resourcePrefab = null;
+        for(int i = 0; i < MaxResources; ++i)
+        {
+            int rnd = Random.Range(1, 5);
+            switch (rnd)
+            {
+                case 1:
+                    resourcePrefab = FoodResource;
+                    break;
+                case 2:
+                    resourcePrefab = WoodResource;
+                    break;
+                case 3:
+                    resourcePrefab = IronResource;
+                    break;
+                case 4:
+                    resourcePrefab = StoneResource;
+                    break;
+            }
+            float x = Random.Range(Corner1.position.x, Corner2.position.x);
+            float z = Random.Range(Corner1.position.z, Corner2.position.z);
+            Vector3 newPos = new Vector3(x, resourcePrefab.transform.position.y, z);
+            Quaternion newRot = Quaternion.Euler(0.0f, Random.Range(0.0f, 360.0f), 0.0f);
+            GameObject go = Instantiate(resourcePrefab, newPos, newRot) as GameObject;
+            go.name = resourcePrefab.name + " (" + (i + 1) + ")";
+            go.transform.parent = resourcePrefab.transform.parent;
+        }
+    }
+}
