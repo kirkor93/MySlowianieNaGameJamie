a78c66ef065ff5852e7962f60b9a5837094bb830
Krzysztof Stengert <krzysztof.stengert@hotmail.com>
2015-07-25 20:42:03 +0200

    Some improvements. Billboard camera renders billboards on top of everything now.

---
+++ Assets/Scripts/BillboardCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BillboardCamera : MonoBehaviour 
+{
+    public Transform MainCamera;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = MainCamera.position;
+        transform.rotation = MainCamera.rotation;
+	}
+}
