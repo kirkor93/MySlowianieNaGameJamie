9f0e502737a6372f71b6fda965123b5e2ce9da7f
Krzysztof Stengert <krzysztof.stengert@hotmail.com>
2015-07-26 12:40:32 +0200

    Cardzzzzzzzzzzzzzzzzzzzzzzzzzzzz

--- Assets/Scripts/Motivator.cs
+++ Assets/Scripts/Motivator.cs
@@ -32,6 +32,7 @@ public class Motivator : MonoBehaviour {
         _mySpeach = GetComponentInChildren<Text>();
         _basePosition = _myRectTransform.localPosition;
         _destinationPosition = _basePosition + new Vector3(-400f, 0f, 0f);
+        _basePosition += new Vector3(100.0f, 0.0f, 0.0f);
         GameManager.Instance.OnGamePeriodChange += OnGamePeriodChange;
         _collectSpeach = string.Format("You have to collect: {0} food, {1} wood, {2} stone, {3} iron", 
             VillageController.Instance.FoodNeededValue, VillageController.Instance.WoodNeededValue, VillageController.Instance.StoneNeededValue, VillageController.Instance.IronNeededValue);
