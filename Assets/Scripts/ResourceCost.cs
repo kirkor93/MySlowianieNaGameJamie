0e8dbc5aa11cd74406b9f2f180dad467ec398912
Patryk Kochański <patryk.kochanski@hotmail.com>
2015-07-26 11:06:17 +0200

    Costs are smarter now :3 and missing star.cs :3

---
+++ Assets/Scripts/ResourceCost.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public struct ResourceCost {
+
+    public int FoodCost;
+    public int StoneCost;
+    public int IronCost;
+    public int WoodCost;
+
+    public ResourceCost()
+    {
+        FoodCost = 0;
+        StoneCost = 0;
+        IronCost = 0;
+        WoodCost = 0;
+    }
+
+    public ResourceCost(int food, int stone, int iron, int wood)
+    {
+        FoodCost = food;
+        StoneCost = stone;
+        IronCost = iron;
+        WoodCost = wood;
+    }
+}
